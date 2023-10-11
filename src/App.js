39376cae0b96572859aab530a47cd89ae3526605
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-11 19:06:35 +0100

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Homepage';
-import CountryDetails from './components/Countrydetails';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
